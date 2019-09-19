139b458af3547edfe51e4770c155e7c149399c12
atleastzero <megan.obryan52@gmail.com>
2019-09-18 19:58:21 -0700

    Add page 2 functions and tests

--- superheroes.py
+++ superheroes.py
@@ -41,8 +41,16 @@ class Hero:
         self.current_health = self.starting_health
 
     def add_ability(self, ability):
-        ''' Add ability to abilities list '''
+        ''' Add ability to abilities list 
+            ability: Ability Object
+        '''
         self.abilities.append(ability)
+
+    def add_armor(self, armor):
+        '''Add armor to self.armors
+            armor: Armor Object
+        '''
+        self.armors.append(armor)
 
     def attack(self):
         '''Calculate the total damage from all ability attacks.
@@ -53,13 +61,71 @@ class Hero:
             total_damage += ability.attack()
         return total_damage
 
+    def defend(self, damage_amt):
+        '''Runs `block` method on each armor.
+            Returns sum of all blocks
+        '''
+        total_block = 0
+        for armor in self.armors:
+            total_block += armor.block()
+        return total_block
+
+    def take_damage(self, damage):
+        '''Updates self.current_health to reflect the damage minus the defense.
+        '''
+        self.current_health = self.current_health - damage + self.defend(damage)
+        
+    def is_alive(self): 
+        '''Return True or False depending on whether the hero is alive or not.
+        '''
+        if self.current_health > 0:
+            return True
+        return False
+
+    def fight(self, opponent):  
+        ''' Current Hero will take turns fighting the opponent hero passed in.
+        '''
+        my_turn = True
+        while self.is_alive() and opponent.is_alive():
+            if my_turn:
+                opponent.take_damage(self.attack())
+                my_turn = False
+            else:
+                self.take_damage(opponent.attack())
+                my_turn = True
+
+        if my_turn: 
+            return opponent.name + " won!"
+        else:
+            return self.name + " won!"
+
 if __name__ == "__main__":
     #ability = Ability("Debugging Ability", 20)
     #print(ability.name)
     #print(ability.attack())
-    ability = Ability("Great Debugging", 50)
-    another_ability = Ability("Smarty Pants", 90)
-    hero = Hero("Grace Hopper", 200)
-    hero.add_ability(ability)
-    hero.add_ability(another_ability)
-    print(hero.attack())
+    #ability = Ability("Great Debugging", 50)
+    #another_ability = Ability("Smarty Pants", 90)
+    #hero = Hero("Grace Hopper", 200)
+    #hero.add_ability(ability)
+    #hero.add_ability(another_ability)
+    #shield = Armor("Shield", 50)
+    #hero.add_armor(shield)
+    #hero.take_damage(50)
+    #print(hero.attack())
+    #print(hero.current_health)
+    #hero.take_damage(150)
+    #print(hero.is_alive())
+    #hero.take_damage(15000)
+    #print(hero.is_alive())
+
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    print(hero1.fight(hero2))
